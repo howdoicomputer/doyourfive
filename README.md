fb8cad864c8f7e974a35217fc927cbdda4adcec1
Tyler Hampton <dr.frankinfurter@gmail.com>
2015-12-28 11:16:06 -0800

    A basic working example.

---
+++ README.md
@@ -0,0 +1,16 @@
+# About
+
+Do Your Five is a small Java based API that tracks someone doing a stripped 5x5 starting strength workout. It aims to fulfill the goal of being written in a concise and readable manner as well as giving me a chance to become more familiar with Java 8, Spark, and Lombok.
+
+## Installation and Getting Started
+
+* Install `maven`
+* `mvn clean compile assembly:single`
+* `java -jar target\doyourfive-1.0-SNAPSHOT-jar-with-dependencies.jar`
+
+## Junit
+* `mvn test`
+
+## Deploying to Heroku
+
+This is a todo.
--- src/main/java/com/howdoicomputer/doyourfive/DoYourFive.java
+++ src/main/java/com/howdoicomputer/doyourfive/DoYourFive.java
@@ -1,33 +1,127 @@
 package com.howdoicomputer;
 import static spark.Spark.*;
 import com.google.gson.Gson;
-import com.fasterxml.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
- * A simple REST API to track my stripped 5x5 workout
+ * A simple POST API to track a stripped 5x5 workout
  *
  */
- public class DoYourFive {
-    public static class DumbbellLift {
-      @Getter @Setter
-      public Int reps, sets;
-    }
+public class DoYourFive {
+
+  /*
+  A small class to represent a workout
+
+  The @Data and @Accessors annotations allow for shorthand declarations of
+  getters, setters, and method chaining.
+
+  See https://projectlombok.org/features/GetterSetter.html
+  */
+  @Data
+  @Accessors(chain=true)
+  public static class Workout {
+
+    private int id;
+    private int sets = 5;
+    private int reps = 5;
+    private String title;
+    private char plan;
+
   }
 
-  public int logWorkout(String id, Int reps, Int sets) {
-    WorkoutModel workout = new WorkoutModel()
+  /*
+  The workout session tracks, well, a workout session.
+  */
+  @Data
+  @Accessors(chain=true)
+  public static class WorkoutSession {
+
+    private int id;
+    private char plan;
+    private HashMap<int,Workout> trackSets;
 
   }
 
   public static void main(String[] args) {
 
-    private static final int HTTP_BAD_REQUEST = 400;
+    /*
+    A bunch of static objects that won't really change
+    */
+    Workout barbellDeadlift = new Workout()
+      .setId(1)
+      .setTitle("Barbell Deadlift")
+      .setPlan('A');
 
-    get("/", (request, response) -> "Welcome!");
+    Workout barbellBentOverRow = new Workout()
+      .setId(2)
+      .setTitle("Barbell Bent Over-Row")
+      .setPlan('A');
 
-    get("/hello", (request, response) -> "Hello!");
+    Workout dumbellShoulderPress = new Workout()
+      .setId(3)
+      .setTitle("Dumbell Shoulder Press")
+      .setPlan('A');
 
+    Workout barbellSquat = new Workout();
+    barbellSquat.setId(4)
+      .setTitle("Barbell Squat")
+      .setPlan('B');
+
+    Workout dumbellBenchPress = new Workout()
+      .setId(5)
+      .setTitle("Dumbell Bench Press")
+      .setPlan('B');
+
+    /*
+    This workout differs in reps and sets values since you're supposed to do
+    two reps at maximum sets. I'm using a -1 to represent infinity because I'm
+    lazy.
+    */
+    Workout pullUp = new Workout()
+      .setId(6)
+      .setTitle("Max Rep Pull Up")
+      .setPlan('B')
+      .setReps(2)
+      .setSets(-1);
+
+    /*
+    I started with a double brace initializer but the ArrayList would come up
+    as null when being serialized so I created this immutable list. Not like
+    the workouts will change, anyway.
+    */
+    List<Workout> workoutList = Arrays.asList(
+      barbellDeadlift,
+      barbellBentOverRow,
+      dumbellShoulderPress,
+      barbellSquat,
+      dumbellBenchPress,
+      pullUp
+    );
+
+    /*
+    Provide the list of available workouts
+    */
+    get("/workouts", (request, response) -> {
+      Gson gson = new Gson();
+      return gson.toJson(workoutList);
+    });
+
+    /*
+    Increment a workout set
+    */
+    post("/workout/:session/:plan/increment", (request, response) -> {
+      Gson gson = new Gson();
+
+    }
   }
+
 }
